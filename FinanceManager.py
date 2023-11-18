c3b64db8db581c99d51c5197095f96caabae3d3a
Hyeji <108911726+hyejidaimma@users.noreply.github.com>
2023-11-18 19:23:17 +0900

    행,열데이터  누락 복구

--- FinanceManager.py
+++ FinanceManager.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 class FinanceManager:
     def __init__(self):
-        pass
+        self.data = None
 
     def get_sise_years(self, code,num_years=3):
         import datetime
@@ -36,7 +36,7 @@ class FinanceManager:
                 '종가': data[4],
             }
             data_dict_list.append(data_dict)
-        return data_dict_list
+        self.data = pd.DataFrame(data_dict_list)
 
     def get_sise_days(self, code, days=30):
         import datetime
@@ -67,29 +67,61 @@ class FinanceManager:
                 '종가': data[4],
             }
             data_dict_list.append(data_dict)
-        return data_dict_list
+        self.data = pd.DataFrame(data_dict_list)
+
 
     def list_to_df(self, data):
         df = pd.DataFrame(data)
         df = df.rename(columns={'날짜': 'Date', '시가': 'Open', '고가': 'High', '저가': 'Low', '종가': 'Close'})
-        return df
+        self.data = df
 
-    def save_to_csv(self, df, filename):
-        df.to_csv(filename, index=False)
+    def save_to_csv(self, filename):
+        self.data.to_csv(filename, index=False)
 
     def load_from_csv(self, filename):
-        df = pd.read_csv(filename)
-        return df
+        self.data = pd.read_csv(filename)
+
+    def get_column_data(self, column_name):
+        if self.data is not None:
+            if column_name in self.data.columns:
+                return self.data[column_name]
+            else:
+                return f"Column '{column_name}' not found in the DataFrame."
+        else:
+            return "DataFrame is empty. Please load or create data first."
+
+    def get_row_by_date(self, date):
+        if self.data is not None:
+            date_row = self.data[self.data['날짜'] == date]
+            if not date_row.empty:
+                return date_row
+            else:
+                return f"No data for the date '{date}'."
+        else:
+            return "DataFrame is empty. Please load or create data first."
 
 if __name__ == "__main__":
     fm = FinanceManager()
-    data = fm.get_sise_days('005930',15)
+    fm.get_sise_years('005930',1)
 
-    df = fm.list_to_df(data)
-    
     # CSV 파일로 저장
-    fm.save_to_csv(df, 'stock_data.csv')
-    
+    fm.save_to_csv('stock_data.csv')
+
     # CSV 파일 불러오기
-    loaded_df = fm.load_from_csv('stock_data.csv')
-    print(loaded_df)
+    fm.load_from_csv('stock_data.csv')
+
+    # 열 데이터 가져오기
+    column_data_y = fm.get_column_data('시가')  # Example: Get '시가' column
+    column_data_x = fm.get_column_data('날짜')  # Example: Get '시가' column
+
+
+    # 행 데이터 가져오기
+    date_row = fm.get_row_by_date('20230105')  # Example: Get data for the date '20230105'
+
+    # 출력
+
+    print(column_data_y)
+    print(column_data_x)
+
+    if not isinstance(date_row, str):
+        print(date_row)
