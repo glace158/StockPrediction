28ecc1b95491087fec38f97518b8ab5c2d95f451
Hyeji <108911726+hyejidaimma@users.noreply.github.com>
2023-11-18 19:08:21 +0900

    연,일 단위 파라미터로 받기

--- FinanceManager.py
+++ FinanceManager.py
@@ -7,10 +7,41 @@ class FinanceManager:
     def __init__(self):
         pass
 
-    def get_sise_3years(self, code):
+    def get_sise_years(self, code,num_years=3):
         import datetime
         end_time = datetime.date.today()
-        start_time = end_time - datetime.timedelta(days=365 * 3)
+        start_time = end_time - datetime.timedelta(days=365 * num_years)
+        start_time_str = start_time.strftime('%Y%m%d')
+        end_time_str = end_time.strftime('%Y%m%d')
+
+        get_param = {
+            'symbol': code,
+            'requestType': 1,
+            'startTime': start_time_str,
+            'endTime': end_time_str,
+            'timeframe': 'day'
+        }
+        get_param = parse.urlencode(get_param)
+        url = "https://api.finance.naver.com/siseJson.naver?%s" % get_param
+        response = requests.get(url)
+        data_list = literal_eval(response.text.strip())
+
+        data_dict_list = []
+        for data in data_list:
+            data_dict = {
+                '날짜': data[0],
+                '시가': data[1],
+                '고가': data[2],
+                '저가': data[3],
+                '종가': data[4],
+            }
+            data_dict_list.append(data_dict)
+        return data_dict_list
+
+    def get_sise_days(self, code, days=30):
+        import datetime
+        end_time = datetime.date.today()
+        start_time = end_time - datetime.timedelta(days)
         start_time_str = start_time.strftime('%Y%m%d')
         end_time_str = end_time.strftime('%Y%m%d')
 
@@ -52,7 +83,8 @@ class FinanceManager:
 
 if __name__ == "__main__":
     fm = FinanceManager()
-    data = fm.get_sise_3years('005930')
+    data = fm.get_sise_days('005930',15)
+
     df = fm.list_to_df(data)
     
     # CSV 파일로 저장
